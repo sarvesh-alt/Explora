d208ce31de7d1474713c09547b2d3851f7da521b
Julie0418 <khatoonjulekha4@gmail.com>
2024-12-02 17:20:26 -0500

    in-progress

--- tour-management/frontend/src/pages/TourDetails.jsx
+++ tour-management/frontend/src/pages/TourDetails.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import '../styles/tour-details.css';
 import { Container, Row, Col, Form, ListGroup } from 'reactstrap';
 import { useParams } from 'react-router-dom';
 import tourData from '../assets/data/tours';
 import calculateAvgRating from './../utils/avgRating';
 import avatar from '../assets/images/avatar.jpg';
+import Booking from '../components/Booking/Booking';
 
 const TourDetails = () => {
 
   const {id} = useParams()
+  const reviewMessageRef = useRef('')
+  const[tourRating, setTourRating] = useState(null)
   const tour = tourData.find(tour => tour.id === id)
   const {photo, title, desc, price, address, reviews, city, distance, maxGroupSize} = tour
   const {totalRating, avgRating} = calculateAvgRating(reviews)
+  const options = {day: 'numeric', month:'long', year:'numeric'}
+  const submitHandler = e=>{
+    e.preventDefault()
+    const reviewText = reviewMessageRef.current.value
+    //api call later
+  }
+
   return <>
   <section>
     <Container>
@@ -25,7 +35,7 @@ const TourDetails = () => {
  
                 <span className="tour__rating d-flex align-items-center gap-1">
                 <i class="ri-star-fill"
-                style={{'color': 'var(--secondary-color)'}}></i> {calculateAvgRating === 0 ? null: 
+                style={{'color': 'var(--secondary-color)'}}></i> {avgRating === 0 ? null: 
                 avgRating}
                 
                 {totalRating === 0 ? ("Not Rated") : (
@@ -41,23 +51,24 @@ const TourDetails = () => {
               <div className="tour__extra-details">
                 <span><i class='ri-map-pin-2-line'></i>{city}</span>
                 <span><i class='ri-money-dollar-circle-line'></i>{price} /per person</span>
-                <span><i class='ri-group-line'></i>{maxGroupSize}</span>
+                <span><i class='ri-map-pin-time-line'></i>{distance}km</span>
+                <span><i class='ri-group-line'></i>{maxGroupSize} people</span>
               </div>
               <h5>Description</h5>
               <p>{desc}</p>
             </div>
             <div className="tour__reviews mt4">
               <h4>Reviews ({reviews?.length} reviews)</h4>
-                <Form>
+                <Form onSubmit={submitHandler}>
                   <div className='d-flex align-items-center gap-3 mb-4 rating__group'>
-                    <span> 1 <i class='ri-star-s-fill'></i></span>
-                    <span> 2 <i class='ri-star-s-fill'></i></span>
-                    <span> 3 <i class='ri-star-s-fill'></i></span>
-                    <span> 4 <i class='ri-star-s-fill'></i></span>
-                    <span> 5 <i class='ri-star-s-fill'></i></span>
+                    <span onClick={()=> setTourRating(1)}> 1 <i class='ri-star-s-fill'></i></span>
+                    <span onClick={()=> setTourRating(2)}> 2 <i class='ri-star-s-fill'></i></span>
+                    <span onClick={()=> setTourRating(3)}> 3 <i class='ri-star-s-fill'></i></span>
+                    <span onClick={()=> setTourRating(4)}> 4 <i class='ri-star-s-fill'></i></span>
+                    <span onClick={()=> setTourRating(5)}> 5 <i class='ri-star-s-fill'></i></span>
                   </div>
                   <div className="review__input">
-                    <input type="text" placeholder="Share your thoughts" />
+                    <input type="text" ref={reviewMessageRef} placeholder="Share your thoughts" />
                     <button className="btn primary__btn text-white" type="submit"> Submit </button>
                   </div>
                 </Form>
@@ -70,9 +81,15 @@ const TourDetails = () => {
                         justify-content-between'>
                           <div>
                             <h5>Julekha</h5>
-                            <p>{new Date('01-12-2024').toLocaleDateString('en-US')}</p>
+                            <p>{new Date('01-12-2024').toLocaleDateString('en-US', options)}</p>
                           </div>
+                          <span className='d-flex align-items-center'>
+                            5<i class='ri-star-s-fill'></i>
+                          </span>
                         </div>
+                        <h6>
+                          Amazing Tour
+                        </h6>
                       </div>
                     </div>
                   ))}
@@ -80,6 +97,9 @@ const TourDetails = () => {
             </div>
           </div>
 
+        </Col>
+        <Col lg='4'>
+         <Booking tour={tour} />
         </Col>
       </Row>
     </Container>
