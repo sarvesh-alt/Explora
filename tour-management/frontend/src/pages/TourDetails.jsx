0460249ffd0012b8196a5695d45e8423b468bb9c
BLURFACEX <76880535+BLURFACEX@users.noreply.github.com>
2024-12-05 11:22:01 -0500

    pushing changes till 31.50

--- tour-management/frontend/src/pages/TourDetails.jsx
+++ tour-management/frontend/src/pages/TourDetails.jsx
@@ -1,80 +1,61 @@
-import React, { useEffect, useRef, useState, useContext } from 'react';
+import React, { useRef, useState } from 'react';
 import '../styles/tour-details.css';
 import { Container, Row, Col, Form, ListGroup } from 'reactstrap';
 import { useParams } from 'react-router-dom';
-import tourData from '../assets/data/tours';
+
 import calculateAvgRating from './../utils/avgRating';
 import avatar from '../assets/images/avatar.jpg';
 import Booking from '../components/Booking/Booking';
-import Newsletter from '../shared/Newsletter';
-
-import {AuthContext} from './../../context/AuthContext'
+import Newsletter from '../shared/Newsletter';  
+import useFetch from '../hooks/useFetch';
+import {BASE_URL} from '../utils/config';
 
 const TourDetails = () => {
-
   const {id} = useParams()
-  const reviewMessageRef = useRef('')
+  const reviewMessageRef = useRef("")
   const[tourRating, setTourRating] = useState(null)
-  const {user} = useContext(AuthContext)
-  const tour = tourData.find(tour => tour.id === id)
+  const {data: tour , loading ,error  } = useFetch('${BASE_URL}/tours/${id}');
   const {photo, title, desc, price, address, reviews, city, distance, maxGroupSize} = tour
   const {totalRating, avgRating} = calculateAvgRating(reviews)
   const options = {day: 'numeric', month:'long', year:'numeric'}
-  const submitHandler = async e=>{
+  
+  const submitHandler = e=>{
     e.preventDefault()
     const reviewText = reviewMessageRef.current.value
+    //api call later
+  };
 
-    
-
-    try {
-      if(!user || user ===undefined || user === null){
-        alert('Please sign in')
-      }
-
-      const reviewObj = {
-        username:user?.username,
-        reviewText,
-        rating: tourRating
-      }
-      const res = await fetch('${BASE_URL}/review/${id}', {
-        method: 'post',
-        headers:{
-          'content-type': 'application/json'
-        },
-        credentials:'include',
-        body: JSON.stringify(reviewObj)
-      }) 
-
-      const result= await res.json()
-      if(!res.ok){
-        return alert(result.message)
-      }
-      alert(result.message)
-    } catch (err) {
-      alert(err.message)
-    }
-    
-  }
+  useEffect(()=> {
+     window.scrollTo(0,0)  
+},[tour])
 
   return <>
   <section>
     <Container>
-      <Row>
+      {
+        loading && <h4 classname = "text-centerpt-5">Loading...</h4> 
+      }
+      {
+        error && <h4 classname = "text-centerpt-5">{error}</h4> 
+      }
+      {
+        !loading && !error &&  <Row>
         <Col lg='8'>
           <div className="tour__content">
             <img src={photo} alt="" />
             <div className="tour__info">
               <h2>{title}</h2>
               <div className='d-flex alight-items-center gap-5'>
+
                 <span className="tour__rating d-flex align-items-center gap-1">
-                  <i 
-                    class="ri-star-s-fill"
-                    style={{color: 'var(--secondary-color)'}}
-                  ></i> 
-                  {avgRating === 0 ? null: avgRating}
-                  {totalRating === 0 ? ("Not Rated") : (
-                  <span>({reviews?.length})</span>
-                  )}   
+                <i class="ri-star-fill"
+                style={{'color': 'var(--secondary-color)'}}></i> {avgRating === 0 ? null: 
+                avgRating}
+
+                {totalRating === 0 ? ("Not Rated") : (
+                <span>({reviews?.length})
+                </span>
+                )}   
                 </span>
 
                 <span>
@@ -113,15 +94,15 @@ const TourDetails = () => {
                         <div className='d-flex align-items-center
                         justify-content-between'>
                           <div>
-                            <h5>{review.username}</h5>
-                            <p>{new Date(review.createdAt).toLocaleDateString('en-US', options)}</p>
+                            <h5>Julekha</h5>
+                            <p>{new Date('01-12-2024').toLocaleDateString('en-US', options)}</p>
                           </div>
                           <span className='d-flex align-items-center'>
-                            {review.rating}<i class='ri-star-s-fill'></i>
+                            5<i class='ri-star-s-fill'></i>
                           </span>
                         </div>
                         <h6>
-                          {review.reviewText}
+                          Amazing Tour
                         </h6>
                       </div>
                     </div>
@@ -132,13 +113,16 @@ const TourDetails = () => {
 
         </Col>
         <Col lg='4'>
-         <Booking tour={tour} avgRating={avgRating} />
+         <Booking tour={tour} />
         </Col>
       </Row>
+
+      }
     </Container>
   </section>
-  <Newsletter />
   </>
 }
 
 export default TourDetails
+
+import tourData from '../assets/data/tours';
