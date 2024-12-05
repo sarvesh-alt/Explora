12678b5230fd52535c6d73aaab87f5754c0d7826
Sarvesh More <sarveshsudhir.more@dcmail.ca>
2024-12-04 23:06:02 -0500

    commit integration
    commited till 1:01:46

--- tour-management/frontend/src/components/Header/Header.jsx
+++ tour-management/frontend/src/components/Header/Header.jsx
@@ -1,10 +1,9 @@
 import React,{useRef,useEffect, useContext  } from 'react';
 import { Container, Row,Button} from 'reactstrap'
-import {NavLink, Link, useNavigate} from 'react-router-dom'
+import {NavLink, Link} from 'react-router-dom'
 
 import logo from '../../assets/images/logo.png'
 import './header.css';
-import {AuthContext} from './../../context/AuthContext'
 
 const nav__links=[
   {
@@ -23,13 +22,14 @@ const nav__links=[
 
 const Header = () => {
 
-  const headerRef = useRef(null)
-  const navigate = useNavigate()
-  const {user, dispatch} = useContext(AuthContext)
+  const headerRef = useRef(null);
+  const menuRef = useRef(null);
+  const navigate = useNavigate();
+  const {user,dispatch} = useContext(AuthContext);
 
-  const logout = ()=> {
-    dispatch({type:'LOGOUT'})
-    navigate('/')
+  const logout = () =>{
+    dispatch({ type: "LOGOUT"});
+    navigate("/")
   }
 
   const stickyHeaderFunc = ()=>{
@@ -48,6 +48,8 @@ const Header = () => {
     return window.removeEventListener("scroll",stickyHeaderFunc);
   });
 
+
+  const toggleMenu = ()=> menuRef.current.classList.toggle('show__menu')
   return (
     <header className="header" ref={headerRef}>
       <Container>
@@ -62,7 +64,7 @@ const Header = () => {
 
 
             {/* ===============menu start=============== */}
-            <div className="navigation">
+            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
               <ul className="menu d-flex align-items-center gap-5">
                {
                 nav__links.map((item,index)=>(
@@ -83,18 +85,10 @@ const Header = () => {
             {/* ===============menu end=============== */}
             <div className="nav__right d-flex align-items-center gap-4">
               <div className="nav__btns d-flex align-items-center gap-4">
-                {
-                  user? (<>
-                  <h5 className='mb-0'>{user.username}</h5>
-                  <Button className='btn btn-dark' onClick={logout}>Logout</Button>
-                  </> ) : (<>
-                  <Button className="btn secondary__btn"><Link to='/login'>Login</Link></Button>
+                    <Button className="btn secondary__btn"><Link to='/login'>Login</Link></Button>
                     <Button className="btn primary__btn"><Link to='/register'>Register</Link></Button>
-                  </>)
-                }
-                    
               </div>
-              <span className="mobile__menu">
+              <span className="mobile__menu" onClick={toggleMenu}>
               <i class="ri-menu-line"></i>
               </span>
             </div>
